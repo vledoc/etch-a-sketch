fe23c2170da86d1b0dc3b1f7b264a9aae58162fd
vledoc <vledoc33@gmail.com>
2019-01-26 15:26:39 -0500

    finished project

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const grid = document.querySelector('.grid');
 const div = document.querySelector('div');
+const newBtn = document.querySelector('#newBtn');
+const partyBtn = document.querySelector('#partyBtn');
 
 function createGrid(size) {
     for (i = 0; i < size; i++) {
@@ -19,6 +21,31 @@ function createGrid(size) {
 
 createGrid(16);
 
-div.addEventListener("mouseover", function(e) {
-    e.target.style.backgroundColor = "black";
-})
+function randomBgColor() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    return 'rgb(' + x + ',' + y + ',' + z + ')';
+}
+
+div.addEventListener('mouseover', function(e) {
+    if (partyBtn.value == 'off') {
+        e.target.style.backgroundColor = 'black';
+    } else {
+        e.target.style.backgroundColor = randomBgColor();
+    }
+});
+
+newBtn.addEventListener('click', (newSize) => {
+    document.getElementsByTagName('div')[0].innerHTML='';
+    var newSize = prompt('How many squares per side would you like?');
+    createGrid(newSize);
+});
+
+partyBtn.addEventListener('click', function() {
+    if (partyBtn.value == 'off') {
+        partyBtn.setAttribute('value', 'on');
+    } else {
+        partyBtn.setAttribute('value', 'off');
+    }
+});
